628754ee1d1e66995aa9de3775e39d372ca3a115
kruemel <weihek@gmail.com>
2018-02-05 13:00:47 +0000

    production status

--- website/config.py
+++ website/config.py
@@ -8,11 +8,11 @@ class Config(object):
     STAGING = False
     PRODUCTION = False
     # on windows:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://kathrin:password@localhost:5433/gamepicker'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://kathrin:password@localhost:5433/gamepicker'
     # on linux:
     #SQLALCHEMY_DATABASE_URI = 'postgresql://kathrin@localhost/gamepicker'
     # on production:
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://apps:apps@localhost/gamepicker'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://apps:apps@localhost/gamepicker'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
