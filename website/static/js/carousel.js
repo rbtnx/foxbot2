f6fc3f0fcab5b6e9353a660d2f2cfd383354b81f
kruemel <weihek@gmail.com>
2018-02-06 15:17:44 +0000

    touchswipe working

--- website/static/js/carousel.js
+++ website/static/js/carousel.js
@@ -27,13 +27,14 @@ $(document).ready(function() {
     $(".carousel-inner").swipe( {
         //Generic swipe handler for all directions
         swipeLeft:function(event, direction, distance, duration, fingerCount) {
-            $(this).parent().carousel('prev'); 
-        },
-        swipeRight: function() {
             $(this).parent().carousel('next'); 
         },
+        swipeRight: function() {
+            $(this).parent().carousel('prev'); 
+        },
+	excludedElements: "label, button, input, select, textarea, .noSwipe",
         //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold:0
+        threshold:1
     });
 });
 // $(document).ready(function() {  
